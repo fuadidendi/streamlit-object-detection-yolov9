2dff754ccc89b0568e1075550ca039527e059ca4
dendi.fuadi <hazikfuadidendi@gmail.com>
2024-03-12 14:34:21 +0700

    feat: update annotation into frame and show realtime using opencv

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
-import cv2 as cv
+from ultralytics.utils.plotting import Annotator
+import cv2
 
 # Load the YOLOv9 model
 print("[INFO] loading YOLOv9 model...")
@@ -7,32 +8,39 @@ model = YOLO('yolov9c.pt')
 
 # initialize the video stream
 print("[INFO] starting video stream...")
-cap = cv.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
-# # Run predictions
-# results = model.predict('test.jpg')
-
-# # Display results
-# results[0].show()
-
 while True:
-     # Capture frame-by-frame
+    # Capture frame-by-frame
     ret, frame = cap.read()
-
+    
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame, stream end? Exiting ...")
         break
 
-    results = model.predict(frame, show=True)
+    results = model.predict(frame)
 
-    if cv.waitKey(1) == ord('q'):
+    for r in results:
+        
+        annotator = Annotator(frame)
+        
+        boxes = r.boxes
+        for box in boxes:
+            
+            b = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
+            c = box.cls
+            annotator.box_label(b, model.names[int(c)])
+
+    frame = annotator.result()
+    cv2.imshow('YOLO V9 Detection', frame)
+    if cv2.waitKey(1) == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
